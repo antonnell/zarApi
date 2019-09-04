130334227af98f8e134dad2821415a9d1f31ed98
Anton Nell <antonnell.crypto@gmail.com>
2019-09-04 08:55:10 +0200

    Removed the new fields. They are breaking the call.
    Signed-off-by: Anton Nell <antonnell.crypto@gmail.com>

--- helpers/zarNetwork.jsx
+++ helpers/zarNetwork.jsx
@@ -29,10 +29,11 @@ const zar = {
   async issue(data, accountDetails, privateKey, callback) {
     try {
       const client = await this.getClient(privateKey)
-      console.log(accountDetails.address, data.name, data.symbol, data.total_supply, !data.mintable, 18, '', data.owner_burnable, data.holder_burnable, data.from_burnable, !data.freezable)
-      const res = await client.issue(accountDetails.address, data.name, data.symbol, data.total_supply, !data.mintable, 18, '', false, false, false, true)
-
       //issue(senderAddress, tokenName, symbol, totalSupply = 0, mintable = false, decimals = "18", description = "", burnOwnerDisabled = false, burnHolderDisabled = false, burnFromDisabled = false, freezeDisabled = false)
+      console.log(accountDetails.address, data.name, data.symbol, data.total_supply, !data.mintable, 18, '', data.owner_burnable, data.holder_burnable, data.from_burnable, !data.freezable)
+
+      const res = await client.issue(accountDetails.address, data.name, data.symbol, data.total_supply, !data.mintable)
+
 
       console.log('***RESPONSE***')
       console.log(res)
